e2fec3218956ec7f34e9437c189f6b8955bf6da2
Samir Kumar <samir.kumar@monkhub.com>
2024-08-17 00:36:34 +0530

    testing 2

--- api/server.js
+++ api/server.js
@@ -1,13 +1,10 @@
 const jsonServer = require('json-server');
 const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router('api/db.json'); // Make sure this path is correct
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use('/api', router);
+server.use(router);
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+module.exports = server;
